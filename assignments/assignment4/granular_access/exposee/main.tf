3ddeb21d3f2399a99440ba6d904f3315cc6d86dc
mslaursen <martin_laursen21@hotmail.com>
2024-03-08 23:42:58 +0100

    .

--- assignments/assignment4/granular_access/exposee/main.tf
+++ assignments/assignment4/granular_access/exposee/main.tf
@@ -7,15 +7,6 @@ terraform {
   }
 }
 
-provider "azurerm" {
-  features {}
-}
-
-resource "azurerm_resource_group" "rg" {
-  name     = "granular-access-rg"
-  location = "westeurope"
-}
-
 resource "azurerm_mysql_server" "mysql" {
   name                = "granular-access-mysql"
   location            = azurerm_resource_group.rg.location
@@ -26,13 +17,22 @@ resource "azurerm_mysql_server" "mysql" {
   version    = "5.7"
 
   administrator_login          = "granular-access"
-  administrator_login_password = "P@ssword1234!"
+  administrator_login_password = "P4ssword1234!"
 
   auto_grow_enabled                 = true
   backup_retention_days             = 7
   geo_redundant_backup_enabled      = true
   infrastructure_encryption_enabled = true
-  public_network_access_enabled     = false
+  public_network_access_enabled     = true
   ssl_enforcement_enabled           = true
   ssl_minimal_tls_version_enforced  = "TLS1_2"
+}
+
+
+resource "azurerm_mysql_database" "mysql" {
+  name                = "granular-access-db"
+  resource_group_name = azurerm_resource_group.rg.name
+  server_name         = azurerm_mysql_server.mysql.name
+  charset             = "UTF8"
+  collation           = "UTF8_GENERAL_CI"
 }
